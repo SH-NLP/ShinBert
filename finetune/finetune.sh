fdb8f28b56774bda57b3c9b8ba395c96767ef02c
Jongyeol Yang <yangjy0113@gmail.com>
2020-12-17 15:18:03 +0900

    wip

---
+++ finetune/finetune.sh
@@ -0,0 +1,7 @@
+python3 run_seq_cls.py --task nsmc --config_file bert-base.json
+python3 run_seq_cls.py --task kornli --config_file bert-base.json
+python3 run_seq_cls.py --task paws --config_file bert-base.json
+python3 run_seq_cls.py --task question-pair --config_file bert-base.json
+python3 run_seq_cls.py --task korsts --config_file bert-base.json
+python3 run_ner.py --task naver-ner --config_file bert-base.json
+python3 run_squad.py --task korquad --config_file bert-base.json
